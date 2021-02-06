40a6127d31ac2f1068bbe5be8041f7a6c0014937
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2021-02-06 20:22:12 +0100

    added load cell

---
+++ esp32/nodeMCU/load_cell.py
@@ -0,0 +1,22 @@
+# scales test
+
+
+from machine import Pin
+
+from time import sleep, sleep_ms
+import time
+from hx711 import HX711
+
+from machine import freq
+freq(160000000)
+
+# connections
+SCK = 27
+DT = 26
+
+print('Testing load cell')
+driver = HX711(d_out=DT, pd_sck=SCK)
+
+while True:
+    val = driver.read()
+    print(val)
