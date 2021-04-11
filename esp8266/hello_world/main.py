44763f2bc849df693ca305b986f8098f8a5573d3
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2021-04-12 00:00:08 +0200

    added led

--- esp8266/hello_world/main.py
+++ esp8266/hello_world/main.py
@@ -1,8 +1,20 @@
+
+from machine import Pin, Signal
 from time import sleep
 
-idx = 0
 
-while True:
-    print('hello there! # %i' %idx)
-    idx += 1
-    sleep(1)
+def main():
+
+    led_pin = Pin(2, Pin.OUT)
+    led = Signal(led_pin, invert=True)
+
+    idx = 0
+    while True:
+        print('hello there! # %i' % idx)
+        led.value(not led.value())
+        idx += 1
+        sleep(1)
+
+
+print('starting main')
+main()
