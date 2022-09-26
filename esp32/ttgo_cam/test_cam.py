1a7f431020697db5ffb413081322e16162083f45
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2022-09-26 11:59:22 +0200

    image test

--- esp32/ttgo_cam/test_cam.py
+++ esp32/ttgo_cam/test_cam.py
@@ -1,2 +1,10 @@
 
+import camera
 print("Hello from camera.")
+
+
+camera.init(0, d0=5, d1=14, d2=4, d3=15, d4=18, d5=23, d6=36, d7=39,
+            format=camera.JPEG, framesize=camera.FRAME_VGA, fb_location=camera.PSRAM,
+            xclk_freq=camera.XCLK_20MHz,
+            href=25, vsync=27, reset=-1, pwdn=-1,
+            sioc=12, siod=13, xclk=32, pclk=19)
